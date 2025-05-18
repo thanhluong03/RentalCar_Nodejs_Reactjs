c47d04971e139302780cdced70e46ead5a97e0d1
thanhluong03 <nguyenthanhluonghh9@gmail.com>
2025-05-19 00:57:52 +0700

    feat(gm-08): login, homheader, home: add navigation when customer logs into the system

--- reactjs/src/containers/HomePage/HomeHeader.js
+++ reactjs/src/containers/HomePage/HomeHeader.js
@@ -4,19 +4,26 @@ import './HomeHeader.scss';
 import logo from '../../assets/images/logo.png';
 import { FormattedMessage } from 'react-intl';
 import { withRouter } from 'react-router';
+import * as actions from '../../store/actions';
 
 class HomeHeader extends Component {
-
     constructor(props) {
         super(props);
         this.state = {
             activeItem: '',
-            searchQuery: ''
+            searchQuery: '',
+            isDropdownOpen: false,
         };
+        this.dropdownRef = React.createRef();
     }
 
     componentDidMount() {
         this.updateActiveItem(this.props.location.pathname);
+        document.addEventListener('click', this.handleClickOutside);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('click', this.handleClickOutside);
     }
 
     componentDidUpdate(prevProps) {
@@ -26,7 +33,6 @@ class HomeHeader extends Component {
     }
 
     updateActiveItem = (path) => {
-        // Tùy chỉnh logic nếu cần
     };
 
     returnToHome = () => {
@@ -49,76 +55,110 @@ class HomeHeader extends Component {
 
     handleSearchChange = (event) => {
         this.setState({ searchQuery: event.target.value });
-    }
+    };
 
-handleSearchSubmit = () => {
-    const { searchQuery } = this.state;
-    if (searchQuery.trim() && this.props.history) {
-        this.props.history.push(`/search-car?keyword=${encodeURIComponent(searchQuery.trim())}`);
-    }
-};
-    render() {
+    handleSearchSubmit = () => {
         const { searchQuery } = this.state;
+        if (searchQuery.trim() && this.props.history) {
+            this.props.history.push(`/search-car?keyword=${encodeURIComponent(searchQuery.trim())}`);
+        }
+    };
+
+    toggleDropdown = () => {
+        this.setState((prev) => ({ isDropdownOpen: !prev.isDropdownOpen }));
+    };
+
+    handleClickOutside = (event) => {
+        if (this.dropdownRef.current && !this.dropdownRef.current.contains(event.target)) {
+            this.setState({ isDropdownOpen: false });
+        }
+    };
+
+    handleLogout = () => {
+        this.props.processLogout();
+    };
+
+    render() {
+        const { searchQuery, isDropdownOpen } = this.state;
+        const { isLoggedIn, userInfo } = this.props;
+
         return (
             <React.Fragment>
                 <div className="home-header-container">
                     <div className="home-header-content">
                         <div className="left-content">
                             <div className='blur-bg'></div>
-                            <img
-                                className="header-logo"
-                                src={logo}
-                                alt="Logo"
-                                onClick={this.returnToHome}
-                            />
+                            <img className="header-logo" src={logo} alt="Logo" onClick={this.returnToHome} />
                         </div>
                         <div className="right-content">
                             <div className="right-body">
                                 <div className="search-box">
-                                    <input type="text" placeholder="Tìm kiếm...."
-                                    value={searchQuery}
-                                    onChange={this.handleSearchChange}
-                                    onKeyDown={(e) => {
-                                        if (e.key === 'Enter') this.handleSearchSubmit();
-                                    }}
+                                    <input
+                                        type="text"
+                                        placeholder="Tìm kiếm...."
+                                        value={searchQuery}
+                                        onChange={this.handleSearchChange}
+                                        onKeyDown={(e) => {
+                                            if (e.key === 'Enter') this.handleSearchSubmit();
+                                        }}
                                     />
                                     <button className="search-btn" onClick={this.handleSearchSubmit}>
                                         <i className="fas fa-search"></i>
                                     </button>
                                 </div>
-                                {/* <input className="search-input" type="text" placeholder="Tìm kiếm..." /> */}
-                                <button className="register-btn" onClick={this.handleViewRegister}>
-                                    Đăng ký
-                                </button>
-                                <button className="login-btn" onClick={this.handleViewLogin}>
-                                    Đăng nhập
-                                </button>
+
+                                {!isLoggedIn ? (
+                                    <>
+                                        <button className="register-btn" onClick={this.handleViewRegister}>
+                                            Đăng ký
+                                        </button>
+                                        <button className="login-btn" onClick={this.handleViewLogin}>
+                                            Đăng nhập
+                                        </button>
+                                    </>
+                                ) : (
+                                    <div className="user-menu" ref={this.dropdownRef}>
+                                        <div className="user-name" onClick={this.toggleDropdown}>
+                                            Xin chào, {userInfo?.first_name || 'Người dùng'}
+                                        </div>
+                                        {isDropdownOpen && (
+                                            <div className="user-dropdown">
+                                                <div className="dropdown-item" onClick={this.handleLogout}>
+                                                    Đăng xuất
+                                                </div>
+                                            </div>
+                                        )}
+                                    </div>
+                                )}
                             </div>
                         </div>
                     </div>
                 </div>
-                {this.props.isShowBanner === true &&
+
+                {this.props.isShowBanner === true && (
                     <div className="home-header-banner">
                         <div className="content-up">
                             <div className="title1"><FormattedMessage id="banner.title1" /></div>
                             <div className="title2"><FormattedMessage id="banner.title2" /></div>
                         </div>
                     </div>
-                }
+                )}
             </React.Fragment>
         );
     }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
     return {
         isLoggedIn: state.user.isLoggedIn,
-        userInfo: state.user.userInfo
+        userInfo: state.user.userInfo,
     };
 };
 
-const mapDispatchToProps = dispatch => {
-    return {};
+const mapDispatchToProps = (dispatch) => {
+    return {
+        processLogout: () => dispatch(actions.processLogout()),
+    };
 };
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(HomeHeader));
