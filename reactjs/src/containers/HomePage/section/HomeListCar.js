39f3befd78ab6000d2210885e285b812c16e0e74
thanhluong03 <nguyenthanhluonghh9@gmail.com>
2025-05-21 16:27:38 +0700

    feat(gm-07): rentalcarcontroller, migration, car, rental, user, web, rentalcarservice, header, homelistcar, formrentalcar, search, detailcar, drtaillocation, userservice, actiontye, rentalcaraction: add rental car

--- reactjs/src/containers/HomePage/section/HomeListCar.js
+++ reactjs/src/containers/HomePage/section/HomeListCar.js
@@ -123,25 +123,6 @@ class HomeListCar extends Component {
                             let imageBase64 = item.image
                                 ? Buffer.from(item.image, 'base64').toString('binary')
                                 : '';
-=======
-        if (this.props.history) {
-            this.props.history.push(`/detail-car/${car.id}`);
-        }
-    };
-
-    render() {
-        const { dataCar, visibleRows, selectedCar } = this.state;
-        const columnsPerRow = 4;
-        const carsPerPage = visibleRows * columnsPerRow;
-        const visibleCars = dataCar.slice(0, carsPerPage);
-
-        return (
-            <div className="section-listcarhome">
-                <div className="title-car">Danh sách ô tô</div>
-                <div className="section-container">
-                    <div className="car-grid">
-                        {visibleCars.map((item, index) => {
-                            let imageBase64 = item.image ? new Buffer(item.image, 'base64').toString('binary') : '';
                             return (
                                 <div className="car-item" key={index} onClick={() => this.handleViewDetailCar(item)}>
                                     <div className="bg-image" style={{ backgroundImage: `url(${imageBase64})` }} />
@@ -151,10 +132,6 @@ class HomeListCar extends Component {
                                     </div>
                                     <div className="rental-car">
                                         <button className="rental" onClick={(e) => this.handleRentalClick(item, e)}>
-                                        <button
-                                            className="rental"
-                                            onClick={(e) => this.handleRentalClick(item, e)}
-                                        >
                                             Thuê xe
                                         </button>
                                     </div>
@@ -164,14 +141,6 @@ class HomeListCar extends Component {
                     </div>
                 </div>
 
-                    {carsPerPage < dataCar.length && (
-                        <div className="load-more-container">
-                            <button className="load-more-button" onClick={this.loadMoreCars}>
-                                Xem thêm
-                            </button>
-                        </div>
-                    )}
-                </div>
                 {selectedCar && (
                     <div className="rental-form-overlay">
                         <FormRentalCar
